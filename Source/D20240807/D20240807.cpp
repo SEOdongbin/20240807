3e15b34ba54bec31176661d0719ca8d03fa5923b
SEOdongbin <tjehdqls0623@naver.com>
2024-08-07 14:32:14 +0900

    second commit

---
+++ Source/D20240807/D20240807.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "D20240807.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, D20240807, "D20240807" );
